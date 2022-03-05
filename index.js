cfafdbad34c75cade163aaa4da6ff7fcfb3e6d80
Alex Grafen <alex.grafen@gmail.com>
2022-03-05 16:33:06 +0000

    add function to check notes off

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const output = document.querySelector("output");
 const form = document.querySelector("form");
 const input = document.querySelector("input");
+// const checkbox = document.querySelector(".checkbox");
 
 let numberOfItems = 0;
 
@@ -17,17 +18,31 @@ function addToDoItem(event) {
     const template = document.querySelector("template");
     const domFragment = template.content.cloneNode(true);
 
+    const checkbox =  domFragment.querySelector("input[type=checkbox]");
     const deleteBtn = domFragment.querySelector("button")
 
     domFragment.querySelector("article").setAttribute("id", `item-${numberOfItems}`);
     domFragment.querySelector("h2").textContent = toDoItem;
-
     //deleteBtn.setAttribute("id", `delete-${numberOfItems}`);
     //deleteBtn.addEventListener('click', deleteToDoItem);
+    checkbox.addEventListener('change', noteChecker);
 
     output.appendChild(domFragment)
 
     form.reset();
+}
+
+function noteChecker (e) {
+    console.log('x')
+    console.log(e.target)
+    if (e.target.checked) {
+        console.log('check')
+        
+        document.querySelector(`#item-${numberOfItems} > h2`).classList.add("checked");
+    }
+    else{
+        document.querySelector(`#item-${numberOfItems} > h2`).classList.remove("checked");
+    }
 }
 
 /* 
